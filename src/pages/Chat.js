e2082d14800d155b514831a822f2bfb3c11312d6
Javon Hickmon <jj.hickmon@gmail.com>
2022-10-23 01:28:34 -0700

    Added test chat feature

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,71 @@
 import '../style/Chat.css';
+import {ChatEngine, ChatFeed} from 'react-chat-engine';
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const now = new Date().getTime();
+const username = ['user', now].join('-');
+
+class Message extends Component{
+
+  render () {
+      return ( 
+        <div > { this.props.uuid }: { this.props.text } 
+        </div>
+      );
+  }
+};
 
 function Chat() {
+
+    // const didMountRef = useRef(false)
+    // const [current, setCurrent] = useState(null)
+    // const { currentUser, setCurrentUser, setUsers } = useContext(Context)
+
+    // function syncUsers() {
+    //     getUsers(users => {
+    //         console.log('Fetched users', users)
+            
+    //         {/* Run this code to load Chat Engine users */}
+    //         currentUsers.map(current => {
+    //             if(!users.find(user => current.username === user.username)) {
+    //                 console.log('Creating user', current.username)
+    //                 createUser(current)
+    //             }
+    //         })
+
+    //         setUsers(users)
+    //         setCurrentUser(currentUsers[0])
+    //     })
+    // }
+
+    // useEffect(() => {
+    //     if (!didMountRef.current) {
+    //         didMountRef.current = true
+    //         syncUsers()
+    //     }
+    // })
+
   return (
     <div className="Chat">
         <h1>Testing Chat Page</h1>
-        {/* Write your html code here!! */}
+
+        <ChatEngine 
+            projectID='707f0546-7b8c-4a4a-9a27-9a27a6f01301'
+            userName='Test'
+            userSecret='testtest'
+        />
+        {/* <ChatEngineWrapper>
+            <ChatSocket 
+                projectID='707f0546-7b8c-4a4a-9a27-9a27a6f01301'
+                chatID='134918'
+                chatAccessKey='ca-6aba8df4-9ba9-4717-b40a-d0aa3c11e2bc'
+                senderUsername='Test'
+            />
+
+            <ChatFeed activeChat='134918' />
+        </ChatEngineWrapper> */}
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,32 @@ function Home() {
   return (
     <div className="Home">
         <h1>Home Page</h1>
-        {/* Write your html code here!! */}
+        <html>
+        <head>
+        <style>
+
+          border: 1px solid black;
+          margin-top: 100px;
+          margin-bottom: 100px;
+          margin-right: 150px;
+          margin-left: 80px;
+          background-color: lightblue;
+
+        </style>
+        </head>
+        <body>
+
+        <h2>Using individual margin properties</h2>
+
+        <div>This div element has a top margin of 100px, a right margin of 150px, a bottom margin of 100px, and a left margin of 80px.</div>
+
+        </body>
+        </html>
+        <div class="container">
+        <img src="https://cdn.discordapp.com/attachments/1033472864030052482/1033516407222186015/Screen_Shot_2022-10-22_at_4.03.34_PM.png" alt="main"></img>
+        <button class="btn">Get Started</button>
+        style="background-color:#fceee9"
+        </div>
     </div>
   );
 }
