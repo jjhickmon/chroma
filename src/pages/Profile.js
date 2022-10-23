85485e9ce8acad226e123a895d8a2a98a699a7bc
qnho <qnho@uw.eduM>
2022-10-22 22:15:42 -0700

    editing profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,7 +4,40 @@ function Profile() {
   return (
     <div className="Profile">
         <h1>Profile Page</h1>
-        {/* Write your html code here!! */}
+        <div>
+          <section id='info'>
+            <p>Email</p>
+            <p>Date of Birth</p>
+            <p>Address</p>
+          </section>
+
+          <span id='nav'>
+            <span>About</span>
+            <span>Work</span>
+            <span>Activity</span>
+          </span>
+
+          <section id='bio'>
+            <p>BIO</p>
+          </section>
+
+          <section id='social'>
+            <p>ON THE WEB</p>
+            <div>
+              <img src='https://i.pinimg.com/originals/5c/a9/8c/5ca98c73b2bb7a02bf8350933c7ca443.png'
+              alt='twitter icon'></img>
+              <img src='https://cdn3.iconfinder.com/data/icons/glypho-social-and-other-logos/64/logo-facebook-512.png'
+              alt='facebook icon'></img>
+              <img src='https://www.logo.wine/a/logo/Instagram/Instagram-Glyph-Black-Logo.wine.svg'
+              alt='instagram icon'></img>
+            </div>
+          </section>
+
+          <section id='personal'>
+            <span>WEBSITE</span>
+            <span>PHONE</span>
+          </section>
+        </div>
     </div>
   );
 }
