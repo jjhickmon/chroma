00f91d7d7ecca17fae477b2e7c05aaef3630f1dd
Javon Hickmon <jj.hickmon@gmail.com>
2022-10-22 21:35:07 -0700

    Testing pushing to GitHub

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -3,7 +3,7 @@ import '../style/Chat.css';
 function Chat() {
   return (
     <div className="Chat">
-        <h1>Chat Page</h1>
+        <h1>Testing Chat Page</h1>
         {/* Write your html code here!! */}
     </div>
   );
--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -3,7 +3,7 @@ import '../style/Questions.css';
 function Questions() {
   return (
     <div className="Questions">
-        <h1>Questions Page</h1>
+        <h1 className="title">Questions Page</h1>
         {/* Write your html code here!! */}
     </div>
   );
